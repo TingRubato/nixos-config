e91edc54bb865710b63f36ed1e993066d9833f00
Tim_Xu <timtsui@outlook.com>
2025-06-23 17:29:24 -0500

    Initial commit

---
+++ modules/darwin/casks.nix
@@ -0,0 +1,21 @@
+_:
+
+[
+  # Development Tools
+  "homebrew/cask/docker"
+  "visual-studio-code"
+  # Communication Tools
+  "discord"
+  "notion"
+  # "slack"
+  # "telegram"
+  "zoom"
+
+  # Entertainment Tools
+  "iina"
+
+  # Productivity Tools
+  "raycast"
+  # Browsers
+  "google-chrome"
+]
---
+++ modules/darwin/packages.nix
@@ -0,0 +1,8 @@
+{ pkgs }:
+
+with pkgs;
+let shared-packages = import ../shared/packages.nix { inherit pkgs; }; in
+shared-packages ++ [
+  dockutil
+  pkgs.awscli  # ✅ use as a Homebrew Formula
+]
